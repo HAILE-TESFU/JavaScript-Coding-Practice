de623e19b432870dbb6d484a507fa746a848b0f4
Haile Tesfu <hailewtesfu@gmail.com>
2025-01-16 00:52:15 -0800

    I have made some changes to coding problem and woks fine

--- Detect Pangram/detectPangram.js
+++ Detect Pangram/detectPangram.js
@@ -1,62 +1,14 @@
 function isPangram(string) {
-  let alphabets = [];
-//   const realAlphabetes = [
-//     "a",
-//     "b",
-//     "c",
-//     "d",
-//     "e",
-//     "f",
-//     "g",
-//     "h",
-//     "i",
-//     "j",
-//     "k",
-//     "l",
-//     "m",
-//     "n",
-//     "o",
-//     "p",
-//     "q",
-//     "r",
-//     "s",
-//     "t",
-//     "u",
-//     "v",
-//     "w",
-//     "x",
-//     "y",
-//     "z",
-//   ];
-
-  const givenString = string.split("");
-  console.log(givenString);
-
-  givenString.forEach((item, i, arr) => {
-    givenString.forEach((itemAlphabet, iAlphabet, arrAlphabet) => {
-      if (item === itemAlphabet || item === " ") {
-        // console.log(item)
-       givenString.shift()
-       alphabets = givenString;
-      }
-    });
-  });
-  
-  console.log(alphabets);
-  if (alphabets.length === 26) {
-    return true;
-  } else return false;
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const normalizedSentence = string.toLowerCase().replace(/[^a-z]/g, '');
+  console.log(normalizedSentence)
+  for(let char of alphabet){
+    if(!normalizedSentence.includes(char)){
+        return false
+    }
+  }
+return true
 }
-
-// function chechAlphabet(alpha) {
-//     alphabets.forEach((item, i, arr) => {
-//       if (arr.length === 0) {
-//         alphabets.push(item);
-//       } else if (alpha !== item) {
-//         alphabets.push(alpha);
-//       }
-//     });
-//   }
 
 const string1 = "The quick brown fox jumps over the lazy dog";
 console.log(isPangram(string1));
--- Detect Pangram/readMe.md
+++ Detect Pangram/readMe.md
@@ -8,3 +8,4 @@
 
     Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
+*/3
